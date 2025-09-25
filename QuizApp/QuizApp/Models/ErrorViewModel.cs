a11f579b6a186db9f1d687c7a15acc54f2a6231d
astlan2500 <143513354+astlan2500@users.noreply.github.com>
2025-09-25 14:44:41 +0200

    slettet test

--- QuizApp/QuizApp/Models/ErrorViewModel.cs
+++ QuizApp/QuizApp/Models/ErrorViewModel.cs
@@ -5,10 +5,4 @@ public class ErrorViewModel
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-//hallo hallo
-public class Astrid_tester_gitt{
-    //astrid tester å merge git
-    public bool ShowRequestId =>
-
-}
+} 
