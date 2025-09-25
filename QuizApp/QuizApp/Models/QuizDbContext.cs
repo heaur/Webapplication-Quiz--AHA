4a0eb6b9960eec31a0770e7999c75fab03a9f762
astlan2500 <143513354+astlan2500@users.noreply.github.com>
2025-09-25 18:56:53 +0200

    tabell til db

--- QuizApp/QuizApp/Models/QuizDbContext.cs
+++ QuizApp/QuizApp/Models/QuizDbContext.cs
@@ -7,6 +7,11 @@ namespace QuizApp.Models;
         {
             Database.Ensurecreated();
         }
+        public DbSet<Question> Questions {get; set; }
+        public DbSet<Option>   Options   { get; set; }
+        public DbSet<Quiz>     Quizzes   { get; set; }
+        public DbSet<User>     Users     { get; set; }
+        public DbSet<Result>   Results   { get; set; }
     }
 
 
