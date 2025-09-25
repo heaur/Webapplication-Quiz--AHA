ff9f75774608b64411f65d5becb8d2c425b64305
astlan2500 <143513354+astlan2500@users.noreply.github.com>
2025-09-25 18:54:04 +0200

    Laget klasse QuizDBContext og lagt til db hvis den ikke finnes som han gjør i demoen

--- QuizApp/QuizApp/Models/QuizDbContext.cs
+++ QuizApp/QuizApp/Models/QuizDbContext.cs
@@ -1,4 +1,17 @@
 using System.Reflection.Metadata;
 using Microsoft.EntityFrameworkCore;
 namespace QuizApp.Models;
+{
+    public class QuizDBContext : DbContext{
+        public QuizDBContext(DbContextOptions<>options): base(options)
+        {
+            Database.Ensurecreated();
+        }
+    }
+
+
+
+
+
+}
 
