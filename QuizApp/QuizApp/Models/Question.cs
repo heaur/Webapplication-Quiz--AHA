e5bd11a8d0a08a39a4758c2ad474d0887b6decd2
astlan2500 <143513354+astlan2500@users.noreply.github.com>
2025-09-25 19:23:08 +0200

    modellen for tabellen Question

--- QuizApp/QuizApp/Models/Question.cs
+++ QuizApp/QuizApp/Models/Question.cs
@@ -6,7 +6,19 @@ namespace QuizApp.Models
 {
     public class Questions
     {
-        [Key]
-        public int OptionId { get; set; }
+         [Key]                                // PK
+        public int QuestionId { get; set; }
+
+        [Required]                           // Må høre til quiz
+        public int QuizId { get; set; }
+
+        [Required]                           // tekst kan ikke være tom
+        [MaxLength(500)]                     // Maks 500 tegn
+        public string Text { get; set; }
+
+        
+
+
+
     }
 }
