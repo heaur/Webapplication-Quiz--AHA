ee0b2820543312c741ab723496f6205eefd16e79
astlan2500 <143513354+astlan2500@users.noreply.github.com>
2025-09-25 19:13:58 +0200

    laget klasse og pk til questions

--- QuizApp/QuizApp/Models/Question.cs
+++ QuizApp/QuizApp/Models/Question.cs
@@ -1,2 +1,12 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+namespace QuizApp.Models
+
+
+{
+    public class Questions
+    {
+        [Key]
+        public int OptionId { get; set; }
+    }
+}
