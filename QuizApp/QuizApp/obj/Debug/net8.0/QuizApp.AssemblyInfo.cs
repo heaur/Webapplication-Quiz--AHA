5a4c4dd796cc6712443414ab1e008985df75c335
heaur <heaur5362@oslomet.no>
2025-10-02 10:52:58 +0200

    test

--- QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
+++ QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7695fdad6b2dcd649740cfe0478f2e07f68226fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6991946aaf193f5c390ca25dde5c04f8fab3f725")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
