7f9e2e002dcb1ac628feefcb3ea24dbd8dc42f6b
heaur <heaur5362@oslomet.no>
2025-10-09 12:31:57 +0200

    prøvd å kjøre

--- QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
+++ QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09cee6ed8e7dcce572a7cfe47de2b9134cc9c339")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0511f77048a5ffe052472c869e5752dc7c0142")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
