0b73b9f9944a09c6ec0c788e5180593bc81a1981
heaur <heaur5362@oslomet.no>
2025-10-09 12:11:36 +0200

    Opprettet account controller

--- QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
+++ QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aa41d7eec23e5d648679001e710ddb5cb8094c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63e70c9265842dddb04c3085c96b1afd1f0485be")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
