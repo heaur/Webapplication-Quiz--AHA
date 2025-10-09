4d01600f9b574ffa3e212c3b9dd70a899e121e7c
heaur <heaur5362@oslomet.no>
2025-10-09 11:28:55 +0200

    commit før pull

--- QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
+++ QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a4c4dd796cc6712443414ab1e008985df75c335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6971c90ba849c026a2d9e7acc5fe28cde47ef056")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
