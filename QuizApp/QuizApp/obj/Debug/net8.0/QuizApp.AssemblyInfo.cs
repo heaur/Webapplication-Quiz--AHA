bc8fe239fe294f4801004923b90426f4575cc26d
heaur <heaur5362@oslomet.no>
2025-10-11 16:55:16 +0200

    Prøvd å kjøre prosjektet

--- QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
+++ QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32989e750debff6c22575b82cffa7299dac49377")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a91b6524669056d89acbccb33d5a2578e1fcec4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
