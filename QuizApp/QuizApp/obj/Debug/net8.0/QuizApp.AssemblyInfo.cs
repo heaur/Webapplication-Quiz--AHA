cd17a184b5daff9705676da39338bd2c7e09f5ee
astlan2500 <143513354+astlan2500@users.noreply.github.com>
2025-10-16 10:28:23 +0200

    masse feil jeg prøver å fikse

--- QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
+++ QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa5dd2f577fb5dce0bf7f26a0aed770ac5d1a034")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34a9787baa5c3ddc84506ac0030b53ba9d5f4bf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
