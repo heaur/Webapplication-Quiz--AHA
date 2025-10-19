db0f829f8532d865225a6492e6567456a8fe9129
Alisa <alibis8642@oslomet.no>
2025-10-19 13:44:23 +0200

    Opprettelse av DAL Mappe og underfiler

--- QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
+++ QuizApp/QuizApp/obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de52161fcf906f6b3f2c4245ad0a78b72f9e4353")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e82d3b4d7ca20c479eca6a29fb2d35ae4d6764")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
